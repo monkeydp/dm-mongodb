4c502e3619b4eb1f71c09d12f523b481ed1f5b08
iPotato <monkeydpotato@gmail.com>
2019-12-08 21:43:20 +0800

    add GradleWrapperExecutor

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/KodeinConfig.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/KodeinConfig.kt
@@ -8,9 +8,9 @@ import kotlin.properties.Delegates
  * @author iPotato
  * @date 2019/12/4
  */
-var kodein: Kodein by Delegates.notNullSingleton()
+internal var kodein: Kodein by Delegates.notNullSingleton()
 
-fun initKodein(vararg modules: Kodein.Module) {
+internal fun initKodein(vararg modules: Kodein.Module) {
     kodein = Kodein {
         modules.forEach { import(it) }
     }
