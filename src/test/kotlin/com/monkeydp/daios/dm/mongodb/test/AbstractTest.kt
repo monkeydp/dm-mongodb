9fe36be113f440654aa9ac50b9173deff17b58ba
iPotato <monkeydpotato@gmail.com>
2019-12-10 20:04:53 +0800

    remove DmTestdata; add DmHelper, ModuleTestdata

--- src/test/kotlin/com/monkeydp/daios/dm/mongodb/test/AbstractTest.kt
+++ src/test/kotlin/com/monkeydp/daios/dm/mongodb/test/AbstractTest.kt
@@ -1,17 +1,15 @@
 package com.monkeydp.daios.dm.mongodb.test
 
 import com.monkeydp.daios.dm.mongodb.MongodbApp
-import com.monkeydp.daios.dm.mongodb.MongodbVersion
 import com.monkeydp.daios.dm.mongodb.config.kodein
 import com.monkeydp.daios.dm.mongodb.mocker.MongodbCpMocker
 import com.monkeydp.daios.dms.sdk.api.ConnApi
 import com.monkeydp.daios.dms.sdk.conn.Conn
 import com.monkeydp.daios.dms.sdk.dm.DmConfig
-import com.monkeydp.daios.dms.sdk.dm.DmTestdataRegistry
 import com.monkeydp.daios.dms.sdk.exception.handler.IgnoreException
 import com.monkeydp.daios.dms.sdk.request.RequestContext
-import com.monkeydp.tools.exception.inner.PropertyUninitializedException
 import com.monkeydp.tools.config.enableDebugMode
+import com.monkeydp.tools.exception.inner.PropertyUninitializedException
 import com.monkeydp.tools.ext.notNullSingleton
 import org.junit.After
 import org.junit.Before
@@ -28,7 +26,6 @@ internal abstract class AbstractTest {
     companion object {
         init {
             enableDebugMode()
-            DmTestdataRegistry.testDsVersion = MongodbVersion.MONGODB_4_0
             MongodbApp(DmConfig.mock())
         }
     }
