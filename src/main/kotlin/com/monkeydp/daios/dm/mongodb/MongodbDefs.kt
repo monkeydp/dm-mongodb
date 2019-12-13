e3ef75c4d250bf208e0be4f8d18a168525f46b5c
iPotato <monkeydpotato@gmail.com>
2019-12-14 02:03:56 +0800

    kodein component add register items

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbDefs.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbDefs.kt
@@ -3,13 +3,18 @@ package com.monkeydp.daios.dm.mongodb
 import com.monkeydp.daios.dm.base.datasource.AbstractDsDefs
 import com.monkeydp.daios.dm.base.datasource.dsDef
 import com.monkeydp.daios.dm.mongodb.MongodbVersion.MONGODB_4_0
+import com.monkeydp.daios.dms.sdk.datasource.DsDef
+import com.monkeydp.tools.ext.kotlin.toPropValueSetX
 
 /**
  * @author iPotato
  * @date 2019/10/27
  */
-object MongodbDefs : AbstractDsDefs() {
+object MongodbDefs : AbstractDsDefs<DsDef>() {
+    
     val mongodb40def = dsDef {
         version = MONGODB_4_0
     }
+    
+    override fun toSet() = toPropValueSetX<DsDef>()
 }
