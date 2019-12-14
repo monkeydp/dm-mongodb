390ab5b908f7b0a93ce1a8218e8332c03197a92d
iPotato <monkeydpotato@gmail.com>
2019-12-14 21:33:31 +0800

    remove DsDefs

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbDefs.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbDefs.kt
@@ -1,20 +1,17 @@
 package com.monkeydp.daios.dm.mongodb
 
-import com.monkeydp.daios.dm.base.datasource.AbstractDsDefs
 import com.monkeydp.daios.dm.base.datasource.dsDef
 import com.monkeydp.daios.dm.mongodb.MongodbVersion.MONGODB_4_0
-import com.monkeydp.daios.dms.sdk.datasource.DsDef
-import com.monkeydp.tools.ext.kotlin.toPropValueSetX
+import com.monkeydp.daios.dms.sdk.annot.SdkDsDef
 
 /**
  * @author iPotato
  * @date 2019/10/27
  */
-object MongodbDefs : AbstractDsDefs<DsDef>() {
+object MongodbDefs {
     
+    @SdkDsDef
     val mongodb40def = dsDef {
         version = MONGODB_4_0
     }
-    
-    override fun toSet() = toPropValueSetX<DsDef>()
 }
