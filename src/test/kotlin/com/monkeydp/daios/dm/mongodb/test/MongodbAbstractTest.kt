93fca2d15bba176947a4aca548fd4899e876eb29
iPotato <monkeydpotato@gmail.com>
2019-12-14 23:31:10 +0800

    *

--- src/test/kotlin/com/monkeydp/daios/dm/mongodb/test/MongodbAbstractTest.kt
+++ src/test/kotlin/com/monkeydp/daios/dm/mongodb/test/MongodbAbstractTest.kt
@@ -1,21 +1,8 @@
 package com.monkeydp.daios.dm.mongodb.test
 
 import com.monkeydp.daios.dm.mongodb.MongodbApp
-import com.monkeydp.daios.dm.mongodb.config.kodein
-import com.monkeydp.daios.dm.mongodb.mocker.MongodbCpMocker
-import com.monkeydp.daios.dms.sdk.api.ConnApi
-import com.monkeydp.daios.dms.sdk.conn.Conn
 import com.monkeydp.daios.dms.sdk.dm.DmConfig
-import com.monkeydp.daios.dms.sdk.exception.handler.IgnoreException
-import com.monkeydp.daios.dms.sdk.request.RequestContext
 import com.monkeydp.tools.config.enableDebugMode
-import com.monkeydp.tools.exception.inner.PropertyUninitializedException
-import com.monkeydp.tools.ext.kotlin.notNullSingleton
-import org.junit.After
-import org.junit.Before
-import org.kodein.di.Kodein
-import org.kodein.di.generic.instance
-import kotlin.properties.Delegates
 
 /**
  * @author iPotato
