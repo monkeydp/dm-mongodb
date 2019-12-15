f402797ab787510c6a2a3ad5f95928ec8985dc65
iPotato <monkeydpotato@gmail.com>
2019-12-16 04:09:16 +0800

    add KProperty1Filter

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodein.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodein.kt
@@ -1,9 +1,6 @@
 package com.monkeydp.daios.dm.mongodb.config
 
-import com.monkeydp.tools.ext.kotlin.notNullSingleton
-import org.kodein.di.Kodein
 import org.kodein.di.LateInitKodein
-import kotlin.properties.Delegates
 
 /**
  * @author iPotato
