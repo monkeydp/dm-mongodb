4545d84aae21db2fedd44d1e6c099aeac8178911
iPotato <monkeydpotato@gmail.com>
2019-12-22 22:14:38 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodeinComps.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dm.mongodb.config
-
-import com.monkeydp.tools.ext.kodein.component.abstr.AbstractKodeinComps
-
-/**
- * @author iPotato
- * @date 2019/12/15
- */
-object MongodbKodeinComps : AbstractKodeinComps()
