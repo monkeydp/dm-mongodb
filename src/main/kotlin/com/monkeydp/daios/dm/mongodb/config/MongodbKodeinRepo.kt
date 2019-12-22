7b3d42bcdcc7fb317bd9330455adf189add88eec
iPotato <monkeydpotato@gmail.com>
2019-12-23 01:13:45 +0800

    Replace DmKodeinHelper with DmKodeinRepo

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodeinRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dm.mongodb.config
-
-import com.monkeydp.tools.ext.kodein.component.abstr.AbstractKodeinRepo
-
-/**
- * @author iPotato
- * @date 2019/12/15
- */
-internal object MongodbKodeinRepo : AbstractKodeinRepo()
