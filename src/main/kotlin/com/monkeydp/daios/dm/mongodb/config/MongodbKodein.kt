4d98ca81f11fd935a13225090584137cd496547d
iPotato <monkeydpotato@gmail.com>
2019-12-23 21:00:55 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodein.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/config/MongodbKodein.kt
@@ -1,15 +1,10 @@
 package com.monkeydp.daios.dm.mongodb.config
 
+import com.monkeydp.daios.dm.base.config.DmKodeinCompRepo
 import com.monkeydp.daios.dms.sdk.share.kodein.DmKodein
-import com.monkeydp.daios.dms.sdk.share.kodein.kodeinModules
-import com.monkeydp.tools.ext.kodein.KodeinHelper.bindComps
 
 /**
  * @author iPotato
  * @date 2019/12/4
  */
-internal val kodein =
-        DmKodein(true) {
-            importAll(*kodeinModules)
-            bindComps(MongodbKodeinCompRepo.comps)
-        }
+internal val kodein = DmKodein(object : DmKodeinCompRepo() {})
