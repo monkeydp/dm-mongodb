a5f8a7b3c66dcae9acc00c1819f41ac6159f4ac9
iPotato <monkeydpotato@gmail.com>
2019-12-23 20:25:28 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbApp.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mongodb/MongodbApp.kt
@@ -1,9 +1,9 @@
 package com.monkeydp.daios.dm.mongodb
 
-import com.monkeydp.daios.dm.base.AbstractDmApp
 import com.monkeydp.daios.dm.mongodb.config.kodein
-import com.monkeydp.daios.dms.sdk.SdkDmApp
 import com.monkeydp.daios.dms.sdk.datasource.Datasource.MONGODB
+import com.monkeydp.daios.dms.sdk.dm.AbstractDmApp
+import com.monkeydp.daios.dms.sdk.dm.SdkDmApp
 
 /**
  * @author iPotato
